ab649bb5b177ede3b26d0baa98f76677506fef5a
Easha <sabihajahaneasha@gmail.com>
2023-07-13 01:27:29 +0600

    Rating,Model,Profile

--- app/src/main/java/com/example/youtubeactivity/SplashActivity.java
+++ app/src/main/java/com/example/youtubeactivity/SplashActivity.java
@@ -25,7 +25,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity (iHome);
                 finish ();
             }
-        },3000);
+        },2000);
 
 
     }
